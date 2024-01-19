1d66e77a89c3f5abccc78dea554051f3c7f8557a
XalililloMuhibbillayev <135448124+XalililloMuhibbillayev@users.noreply.github.com>
2024-01-19 21:39:05 +0500

    update

--- js/main.js
+++ js/main.js
@@ -16,3 +16,17 @@ function reduce(e) {
 function decrement(e) {
     e.previousElementSibling.innerHTML=parseInt(e.previousElementSibling.innerHTML)+1;
 };
+
+
+
+
+// Initialize the map
+let map = L.map('map').setView([51.505, -0.09], 13);
+
+// Add a tile layer (you can use different tile providers)
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Add a marker
+let marker = L.marker([51.5, -0.09]).addTo(map);
