4752634dd78f4e3d3ff473947a4c1710d3be914a
XalililloMuhibbillayev <135448124+XalililloMuhibbillayev@users.noreply.github.com>
2024-01-20 13:40:13 +0500

    update

--- js/detail.js
+++ js/detail.js
@@ -11,3 +11,20 @@ let detailImage = document.querySelector('.second-detail-image');
 function detailImagge() {
     detailImage.classList.add('active-detail-image')
 }
+
+
+
+// Get the container element
+var btnContainer = document.getElementById("detail-image");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("second-detail-image");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function() {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
