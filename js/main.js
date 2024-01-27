4c59e5ab2f94e549c35ed1ebdbeeece7a701d67f
XalililloMuhibbillayev <135448124+XalililloMuhibbillayev@users.noreply.github.com>
2024-01-27 17:04:21 +0500

    update

--- js/main.js
+++ js/main.js
@@ -18,12 +18,3 @@ function decrement(e) {
 };
 
 
-// Map
-let map = L.map('map').setView([51.505, -0.09], 13);
-
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-let marker = L.marker([51.5, -0.09]).addTo(map);
-
